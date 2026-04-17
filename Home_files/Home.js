71a6cbf079d485ba20a070864beaa75b2c6f0509
gmkohnResearch <64865628+gmkohnGouldian@users.noreply.github.com>
2026-04-17 14:50:26 -0400

    Add files via upload

---
+++ Home_files/Home.js
@@ -0,0 +1,9 @@
+// Created by iWeb 3.0.4 local-build-20260417
+
+setTransparentGifURL('Media/transparent.gif');function applyEffects()
+{var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_0:new IWEmptyStroke()});registry.applyEffects();}
+function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('Home_files/HomeMoz.css')
+fixAllIEPNGs('Media/transparent.gif');applyEffects()}
